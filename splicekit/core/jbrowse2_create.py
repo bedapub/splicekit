1cdf4c26012a6e9b99e3e861dd5c57c74cbcb6fc
Gregor Rot <gregor.rot@gmail.com>
2023-06-07 16:04:25 +0200

    JBrowse2

--- splicekit/core/jbrowse2_create.py
+++ splicekit/core/jbrowse2_create.py
@@ -143,7 +143,7 @@ def process(force_samples=False, force_annotation=False):
     os.system(f'rm -r jobs/jobs_jbrowse/*')
     write_sample_jobs(force_samples) # if force_samples is True, then jobs are created
     # run  sample jobs if available
-    if force_samples:
+    if (not os.path.exists('jbrowse2/splicekit_data/config.json')) or (force_samples):
         if splicekit.config.platform=="cluster":
             os.system('export BSUB_QUIET=Y; jobs=( $(ls jobs/jobs_jbrowse/*.job) ); g=10; for((i=0; i < ${#jobs[@]}; i+=g)); do part=( "${jobs[@]:i:g}" ); for job_fname in ${part[*]}; do echo "[jbrowse] submitted $job_fname"; bsub -K < ${job_fname} & done; wait; echo "[jbrowse] processing next 10"; done; echo "[jbrowse] processing complete"')
         if splicekit.config.platform=="desktop":
