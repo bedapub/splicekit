ea3a7db368f4cced4ab8e31ec78ddaf8e840df26
Gregor Rot <gregor.rot@gmail.com>
2025-02-08 18:26:47 +0100

    install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+# install splicekit
+pip install .
+
+# create conda/mamba environment named splicekit
+micromamba -y create -f splicekit.yaml
+
+#activate
+micromamba activate splicekit
+
+# install R packages
+R -e "install.packages(c('BiocManager', 'data.table', 'statmod', 'R.utils'), repos='https://cloud.r-project.org/')"
+R -e "BiocManager::install('edgeR')"
+
+# install perl packages
+cpanm XML::Compile::Transport::SOAPHTTP XML::Compile::WSDL11 XML::Compile::SOAP11 XML::Compile XML::LibXML::Simple XML::LibXML Log::Log4perl Math::CDF CGI File::Which Data::Dumper File::Copy File::Spec::Functions HTML::PullParser HTML::Template HTML::TreeBuilder JSON Pod::Usage XML::Simple XML::Parser::Expat
+
+# install jbrowse via nodejs
+npm install -g @jbrowse/cli
